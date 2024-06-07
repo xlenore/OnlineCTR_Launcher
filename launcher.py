d6e520f6ab8bd54ef069f286de6e82380681e66b
LeonardoRosales1485 <leonardorosales1485@gmail.com>
2024-06-07 13:39:47 -0300

    Replaced png files to have same dimensions, added some code to combine both images, have a strange warning I cant fix

--- launcher.py
+++ launcher.py
@@ -4,7 +4,7 @@ import re
 import subprocess
 import sys
 import threading
-import tkinter
+from tkinter import Tk, Label, Image
 
 import requests
 from PIL import Image, ImageEnhance, ImageTk
@@ -188,13 +188,20 @@ class GameLauncher:
             self.print_logs("Error launching CTRClient")
             self.print_logs(str(e))
 
+class CTkImage(ImageTk.PhotoImage):
+    def __init__(self, image=None, **kwargs):
+        super().__init__(image, **kwargs)
+        self._image = image
+
+    def __getattr__(self, name):
+        return getattr(self._image, name)
 
 class GameLauncherGUI(customtkinter.CTk):
     def __init__(self, game_launcher):
         super().__init__()
         self.protocol("WM_DELETE_WINDOW", self.on_close)
 
-        self.iconpath = ImageTk.PhotoImage(file='assets\\icon.png')
+        self.iconpath = CTkImage(file='assets\\icon.png')
         self.wm_iconbitmap()
         self.iconphoto(False, self.iconpath)
 
@@ -211,22 +218,22 @@ class GameLauncherGUI(customtkinter.CTk):
     
     def create_widgets(self):
         #Background
-        current_path = os.path.dirname(os.path.realpath(__file__))
-        img = Image.open(current_path + "\\assets\\background.png")
+        background_path = "assets/background.png"
+        self.background_image = Image.open(background_path)
+        self.background_image = self.background_image.convert("RGBA")
+        self.background_image = self.background_image.resize((800, 350), Image.LANCZOS)
 
-        enhancer = ImageEnhance.Brightness(img)
-        darker_img = enhancer.enhance(0.5)
-        
-        self.bg_image = customtkinter.CTkImage(darker_img, size=(800, 350))
-        self.bg_image_label = customtkinter.CTkLabel(self, text=None, image=self.bg_image)
-        self.bg_image_label.place(x=0, y=0)
-        
         #Logo
+        logo_path = "assets/icon.png"
+        self.logo_image = Image.open(logo_path)
+        self.logo_image = self.logo_image.convert("RGBA")
+        self.logo_image = self.logo_image.resize((800, 350), Image.LANCZOS)
 
-        logo_img = Image.open(current_path + "\\assets\\icon.png")
-        self.logo_img = customtkinter.CTkImage(logo_img, size=(200, 200))
-        self.logo_img_label = customtkinter.CTkLabel(self, text=None, image=self.logo_img, bg_color="transparent")
-        self.logo_img_label.place(x=550, y=25)
+        #Combine
+        self.combined_image = Image.alpha_composite(self.background_image, self.logo_image)
+        self.combined_image_tk = CTkImage(self.combined_image)
+        self.background_label = customtkinter.CTkLabel(self, image=self.combined_image_tk)
+        self.background_label.place(x=0, y=0)
         
         
         #Launch Button
@@ -234,11 +241,11 @@ class GameLauncherGUI(customtkinter.CTk):
                                                 fg_color="#951A02", hover_color="#FFC432", border_color="#EE6518",
                                                 border_width=2, width=150, height=50,
                                                 command=game_launcher.launch_game)
-        self.game_launcher_button.place(x=650, y=300)
+        self.game_launcher_button.place(x=550, y=250)
         
         #Logs
         self.logs_text = customtkinter.CTkTextbox(self, font=("", 12),
-                                                width=480, height=250)
+                                                width=480, height=250, border_color="#000000", border_width=2)
         self.logs_text.place(x=10, y=50)
 
 
